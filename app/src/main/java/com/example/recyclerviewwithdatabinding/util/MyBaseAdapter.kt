114d6a02133fd4c376b26cd7b84d43a56ce1050b
Tyler Jackson <tylerj3@gmail.com>
2019-12-09 10:16:17 -0500

    RecyclerView now displaying

--- app/src/main/java/com/example/recyclerviewwithdatabinding/util/MyBaseAdapter.kt
+++ app/src/main/java/com/example/recyclerviewwithdatabinding/util/MyBaseAdapter.kt
@@ -6,15 +6,16 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewwithdatabinding.R
 
-class MyBaseAdapter(val items: List<String>, val context: Context):  RecyclerView.Adapter<MyViewHolder>() {
+class MyBaseAdapter(val dataSet: List<String>, val context: Context):  RecyclerView.Adapter<MyViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         return  MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_layout,parent,false))
     }
 
     override fun getItemCount(): Int {
-        return items.size
+        return dataSet.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.stateName.text = dataSet[position]
     }
 }
--- app/src/main/java/com/example/recyclerviewwithdatabinding/util/MyViewHolder.kt
+++ app/src/main/java/com/example/recyclerviewwithdatabinding/util/MyViewHolder.kt
@@ -1,7 +1,15 @@
 package com.example.recyclerviewwithdatabinding.util
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewwithdatabinding.R
 
 class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    val stateName : TextView
+
+    init {
+        stateName =
+            itemView.findViewById(R.id.stateName)
+    }
 }
